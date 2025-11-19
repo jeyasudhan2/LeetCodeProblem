7fc871131649422e0021c98204486b548aa8d7e1
jeyasudhan2 <jeyasuthan265@gmail.com>
2025-11-19 21:27:53 +0530

    leetcode problem

---
+++ StringLastword.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        spaceCount =0
+        i = len(s)-1
+        while i >= 0 and s[i] == " ":
+            i-=1
+
+
+        while i>=0 and s[i] != " ":
+            spaceCount+=1
+            i-=1
+
+
+        return spaceCount
+
+
+solution = Solution()
+print(solution.lengthOfLastWord("   fly me   to   the moon  "))
